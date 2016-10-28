a0a3b82cc525385ab15e2fff2fe981d2857e1178
binni11 <bro3@hi.is>
2016-10-28 16:06:16 +0000

    Main addad

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@
  * Created by brynjarolafsson on 28/10/16.
  */
 public class Main {
-    //comment 
+    //comment
+    public static void main(String[] args){
+        System.out.println(args[0]);
+    }
 }
