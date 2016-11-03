aaba8bd2448d00abfa9d717ab4ebac022fc472ca
binni11 <bro3@hi.is>
2016-11-03 16:14:25 +0000

    player

--- src/Player.java
+++ src/Player.java
@@ -4,4 +4,15 @@
 
 public class Player {
 
+    public String playerName;
+    Race race;
+
+    public Player(String playerName,Races races){
+        this.playerName = playerName;
+        this.race = new Race(races);
+    }
+
+    public static void main(String[] args) {
+        Player player = new Player("Johnny", Races.Orc);
+    }
 }
