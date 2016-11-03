93959242f6cd1d31d1d4ff82edc1b1b178bcb5e0
Tryggvi Þór Guðmundsson <tryggvithorg@gmail.com>
2016-11-03 16:29:25 +0000

    Added barebones Merchant

---
+++ src/Merchant.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Tryggvi on 03/11/2016.
+ */
+public class Merchant {
+    public class Action {
+
+    }
+
+    public int calculateState(Features features, Action action) {
+        return 0;
+    }
+}
