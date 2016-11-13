3d64bd6f5593f1fe7a6cf117abca4e1c81973a59
hoddi84 <hordurh15@ru.is>
2016-11-13 16:55:53 +0000

    Testing score approximation stuffs

--- src/State.java
+++ src/State.java
@@ -6,7 +6,7 @@ import java.util.Collection;
  */
 public class State {
 
-    public Collection<MerchantMemory> memoryCollection;
+    public ArrayList<MerchantMemory> memoryCollection;
 
     public State() {
 
@@ -15,6 +15,11 @@ public class State {
 
     public void meetPlayer(MerchantMemory memory) {
 
+        if (memoryCollection.isEmpty()) {
+            memoryCollection.add(memory);
+            System.out.println("First time meeting");
+            return;
+        }
         if (!memoryCollection.contains(memory)) {
             System.out.println("I haven't met you before, adding to memory and calculating score");
             memoryCollection.add(memory);
@@ -26,8 +31,50 @@ public class State {
 
     public void listAllMemories() {
         for (MerchantMemory mem : memoryCollection) {
-            System.out.println(mem.features);
+            System.out.println(mem);
         }
+    }
+
+    public double calculatePlayerScore(MerchantMemory memory) {
+
+        ArrayList<Double> list = new ArrayList<>();
+        int counter = 0;
+        double contribution = 0;
+        double playerScore = 0;
+
+        System.out.println("Comparing this: \n" + memory.toString());
+        System.out.println("To the current list:");
+        listAllMemories();
+
+        // iterate the memory list.
+        for (int i = 0; i < memoryCollection.size(); i++) {
+            counter = 0;
+            contribution = 0;
+            System.out.println("Going over memory list item nr: " + i);
+            // iterate the features in the current memory list.
+            for (int k = 0; k < memoryCollection.get(i).features.size(); k++) {
+                // compare the new memory to the features in the memory list.
+                for (int l = 0; l < memory.features.size(); l++) {
+                    if (memory.features.get(l).equals(memoryCollection.get(i).features.get(k))) {
+                        System.out.println("Found match: " + memory.features.get(l));
+                        counter += 1;
+                        int size = memoryCollection.get(i).features.size();
+                        double score = memoryCollection.get(i).playerScore;
+                        contribution += (1D/size)*score;
+                    }
+                }
+            }
+            System.out.println("Found Matches: " + counter + " PlayerScore: " + memoryCollection.get(i).playerScore);
+            System.out.println("Adding to list: " + "i: " + i + " contribution: " + contribution);
+            list.add(i, contribution);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            playerScore += list.get(i);
+        }
+
+        System.out.println("Resulting playerScore: " + playerScore);
+        return playerScore;
     }
 
 }
--- src/Testing.java
+++ src/Testing.java
@@ -10,17 +10,26 @@ public class Testing {
 
         Player player = new Player("Johnny", Races.Orc);
         Player player2 = new Player("Eldur", Races.Goblin);
+        Player player3 = new Player("Vatn", Races.Human);
+        Player player4 = new Player("Earth", Races.Elf);
 
         MerchantMemory newMem = new MerchantMemory(player, PlayerActions.Leave, 30);
         MerchantMemory newMem2 = new MerchantMemory(player2, PlayerActions.Buy, 50);
-        System.out.println(newMem.toString());
+        MerchantMemory newMem3 = new MerchantMemory(player3, PlayerActions.Steal, -40);
+        MerchantMemory newMem4 = new MerchantMemory(player4, PlayerActions.Buy, 60);
+        MerchantMemory newMem5 = new MerchantMemory(player3, PlayerActions.Steal, -100);
+        MerchantMemory newMem6 = new MerchantMemory(player4, PlayerActions.Buy, 35);
 
         State state = new State();
         state.meetPlayer(newMem);
-        state.meetPlayer(newMem2);
-        state.meetPlayer(newMem2);
+        state.meetPlayer(newMem3);
+        state.meetPlayer(newMem4);
+        state.meetPlayer(newMem5);
+        state.meetPlayer(newMem6);
 
-        state.listAllMemories(); 
+        state.listAllMemories();
+
+        state.calculatePlayerScore(newMem2);
 
     }
 }
